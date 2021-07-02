fa372eb5d741a12ca68b049d1bed9758a93a6cbe
chisto4 <chisto4@gmail.com>
2021-07-02 13:12:20 +0300

    hot test work

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ const buttonTest = document.getElementById("changeWrapperDark");
 const dark = document.getElementById("r2d2");
 const choice = document.getElementById("shipping");
 const shipWrapperOne = document.getElementsByClassName("ship_wrapper_one");
+const testShoutButton = document.getElementById("test_shout");
+const clearButton = document.getElementById("clear_button");
 
 
 function buttonTestonClick() {
@@ -17,6 +19,7 @@ function buttonTestonClick() {
   }
 }
 buttonTest.addEventListener("click", _ => buttonTestonClick());
+
 
 //CHOICE SHIP FUNCTION 
 
@@ -57,109 +60,176 @@ changeThreeShip2.addEventListener("click", _ => RedShipBorder(changeThreeShip2))
 
 changeForeShip.addEventListener("click", _ => RedShipBorder(changeForeShip));
 
-
-
-//NEW SHIP
-
-// const allShipWrapper = document.querySelectorAll('.ship_wrapper');
-
-// function clikListen (event, allShipWrapper) {
-//   console.log('Ti najal na kletku');
-//   if(changeShip.style.border === "2px solid red"){
-//   if(event.which == 1){
-//     allShipWrapper.classList.add("ship_wrapper_oneShip");
-//     changeShip.style.display = "none";
-//   } 
-//   else if(event.which == 3){
-//     allShipWrapper.classList.remove("ship_wrapper_oneShip");
-//   }}
-// }
-// allShipWrapper.forEach((el)=>{
-//   el.addEventListener("mousemove", (e)=>{clikListen(e, el);});
-//   el.addEventListener("click", (e)=>{
-//     clikListen(e, el);
-//   });
-// });
-// const allOneShipArr = document.querySelectorAll('div.one_ship');
-// // const allOneShipArrN = allOneShipArr.forEach();
-// console.log(allOneShipArr);
-// const allShipWrapper = document.querySelectorAll('div.ship_wrapper');
-// const countOneShip = 1;
-// // window.asd = allShipWrapper
-
-// function newShip({target:gridElement}) {
-//   // console.log('allShipWrapper', allShipWrapper);
-//   if(allOneShipArr[0].style.border === "2px solid red" ||
-//       allOneShipArr[1].style.border === "2px solid red" ||
-//       allOneShipArr[2].style.border === "2px solid red" ||
-//       allOneShipArr[3].style.border === "2px solid red") {
-//   if (gridElement.classList !== "ship_wrapper_oneShip") {
-//     gridElement.classList.add("ship_wrapper_oneShip");
-//     if(allOneShipArr[0].style.border === "2px solid red"){
-//       allOneShipArr[0].style.border = "none";
-//       allOneShipArr[0].remove();  
-//     }else if(allOneShipArr[1].style.border === "2px solid red"){
-//       allOneShipArr[1].style.border = "none";
-//       allOneShipArr[1].remove();  
-//     }else if(allOneShipArr[2].style.border === "2px solid red"){
-//       allOneShipArr[2].style.border = "none";
-//       allOneShipArr[2].remove();  
-//     }else if(allOneShipArr[3].style.border === "2px solid red"){
-//       allOneShipArr[3].style.border = "none";
-//       allOneShipArr[3].remove();  
-//     }
-    
-//   } else {
-//     gridElement.classList.remove("ship_wrapper_oneShip");
-//   }}
-//   console.log('clik', newShip);
-// }
-
-
-// allShipWrapper.forEach((el)=> {
-//   el.addEventListener('click', newShip)
-//   // console.log(el);
-// })
-
-// let Arr1 = [];
-// const ArrDjedai = push.Arr1(allShipWrapper.forEach());
-// allShipWrapper.addEventListener("click", _ => newShip(allShipWrapper));
-
-// function clikListen (event, allShipWrapper) {
-//   console.log('kletka');
-//   if(changeShip.style.border === "2px solid red"){
-//   if(event.which == 1){
-//     allShipWrapper.classList.add("ship_wrapper_oneShip");
-//     changeShip.style.display = "none";
-//   } 
-//   else if(event.which == 3){
-//     allShipWrapper.classList.remove("ship_wrapper_oneShip");
-//   }}
-// }
-// allShipWrapper.forEach((el)=>{
-//   el.addEventListener("mousemove", (e)=>{clikListen(e, el);});
-//   el.addEventListener("click", (e)=>{
-//     clikListen(e, el);
-//   });
-// });
-
+// CLASS LIST CONSTANTS
 
 const allOneShipArr = document.querySelectorAll('div.one_ship');
-// const allOneShipArrN = allOneShipArr.forEach();
-console.log(allOneShipArr);
-const allShipWrapper = document.querySelectorAll('div.ship_wrapper');
-const newAllShipWrapper = allOneShipArr.filter((item, index) => {
-  return item.style.border === "2px solid red";
-})
-const countOneShip = 1;
-// window.asd = allShipWrapper
+const allTwoShipArr = document.querySelectorAll('div.two_ship');
+const allThreeShipArr = document.querySelectorAll('div.three_ship');
+const foreShipArr = document.querySelectorAll('div.fore_ship');
 
-function newShip({target:gridElement}) {
-  // console.log('allShipWrapper', allShipWrapper);
+//SETKA POLYA DJEDAI & R2D2
+
+const allShipWrapper = document.querySelectorAll('div.ship_wrapper');
+
+const allShipWrapperr2d2 = document.querySelectorAll('div.ship_wrapper_r2d2');
+
+
+
+// FILTER ON DJEDAI GRID!!!
+
+const ArrayGridDj = Array.from(allShipWrapper);
+
+let mapAllShipWrapper = [];
+ArrayGridDj.forEach((node, index) => {
+  if(node.classList.contains('ship_wrapper')) {
+    mapAllShipWrapper.push(index);
+  }
+})
+console.log(mapAllShipWrapper);
+
+//FILTER ZERO
+let mapArrZero = [];
+ArrayGridDj.forEach((node, index) => {
+  if(node.classList.contains('zero')) {
+    mapArrZero.push(index);
+  }
+})
+
+//FILTER NOT WAR
+let mapArrNotWar = [];
+ArrayGridDj.forEach((node, index) => {
+  if(node.classList.contains('notWar')) {
+    mapArrNotWar.push(index);
+  }
+})
+
+//FILTER SHIPS
+let mapArrShipsDj = [];
+ArrayGridDj.forEach((node, index) => {
+  if(node.classList.contains('ship_wrapper_oneShip')) {
+    mapArrShipsDj.push(index);
+  }
+})
+
+//FILTER ONE SHOUT
+let mapArrShotOnGridDj = [];
+ArrayGridDj.forEach((node, index) => {
+  if(node.classList.contains('r2_One_shot')) {
+    mapArrShotOnGridDj.push(index);
+  }
+})
+
+//FILTER CLEAR GRID
+let mapArrClearDj = [];
+ArrayGridDj.forEach((node, index) => {
+  if(!node.classList.contains('ship_wrapper_oneShip') && !node.classList.contains('zero') 
+      && !node.classList.contains('notWar') && !node.classList.contains('r2_One_shot')) {
+        mapArrClearDj.push(index);
+  }
+})
+
+
+// FILTER ON R2D2 GRID!!!
+
+const ArrayGridr2d2 = Array.from(allShipWrapperr2d2);
+
+//FILTER ZERO
+let mapArrZeroR2 = [];
+ArrayGridr2d2.forEach((node, index) => {
+  if(node.classList.contains('zero')) {
+    mapArrZeroR2.push(index);
+  }
+})
+
+//FILTER NOT WAR
+let mapArrNotWarR2 = [];
+ArrayGridr2d2.forEach((node, index) => {
+  if(node.classList.contains('notWar')) {
+    mapArrNotWarR2.push(index);
+  }
+})
+
+//FILTER SHIPS
+let mapArrShipsR2 = [];
+ArrayGridr2d2.forEach((node, index) => {
+  if(node.classList.contains('ship_wrapper_oneShip_r2d2')) {
+    mapArrShipsR2.push(index);
+  }
+})
+console.log('test', mapArrShipsR2)
+
+//FILTER ONE SHOUT
+let mapArrShotOnGridR2 = [];
+ArrayGridr2d2.forEach((node, index) => {
+  if(node.classList.contains('dj_One_shot')) {
+    mapArrShotOnGridR2.push(index);
+  }
+})
+
+
+//FILTER CLEAR GRID
+let mapArrClearR2 = [];
+ArrayGridr2d2.forEach((node, index) => {
+  if(!node.classList.contains('ship_wrapper_oneShip_r2d2') && !node.classList.contains('zero') 
+      && !node.classList.contains('notWar') && !node.classList.contains('dj_One_shot')) {
+        mapArrClearR2.push(index);
+  }
+})
+
+//LIGH VERSION DOSENT WORK
+// const mapGridDjedai = Array.from(allShipWrapper);
+// const mapArrZero = mapGridDjedai.filter((node) => node.classList.contains('zero'))
+//                                 .map((node, index) => index);
+
+
+//TEST FUNCTION WORK WITH ARRAY MAP
+function testZeros(zeroArray) {
+  zeroArray.forEach((value) => {
+    allShipWrapper[value].classList.add("r2_One_shot");
+  })
+}
+testZeros(mapArrZero);
+
+//FINCTION ON PAGE!!!!
+
+//CLEAR FUNCTION
+function clearFunction (clear){
+  clear.forEach((value) => {
+    allShipWrapper[value].className = 'ship_wrapper';
+    allShipWrapperr2d2[value].className = 'ship_wrapper_r2d2';
+  })
+}
+clearButton.addEventListener("click", _ => clearFunction(mapAllShipWrapper));
+
+
+
+
+
+
+
+
+
+
+// window.asd = allShipWrapper
+let newAllShipWrapper;
+
+// newAllShipWrapper = newAlOnB.filter((item, index) => {
+//   return item.style.border === "";
+// })
+// console.log('Red border ship number',newAllShipWrapper);
+
+  function newShip({target:gridElement}) {
   if(allOneShipArr[0].style.border === "2px solid red" ||
       allOneShipArr[1].style.border === "2px solid red" ||
       allOneShipArr[2].style.border === "2px solid red" ||
-      allOneShipArr[3].style.border === "2px solid red") {
+      allOneShipArr[3].style.border === "2px solid red" ||
+      allTwoShipArr[0].style.border === "2px solid red" ||
+      allTwoShipArr[1].style.border === "2px solid red" ||
+      allTwoShipArr[2].style.border === "2px solid red" ||
+      allThreeShipArr[0].style.border === "2px solid red" ||
+      allThreeShipArr[1].style.border === "2px solid red" ||
+      foreShipArr[0].style.border === "2px solid red" )
+      {
   if (gridElement.classList !== "ship_wrapper_oneShip") {
     gridElement.classList.add("ship_wrapper_oneShip");
     if(allOneShipArr[0].style.border === "2px solid red"){
@@ -174,13 +244,33 @@ function newShip({target:gridElement}) {
     }else if(allOneShipArr[3].style.border === "2px solid red"){
       allOneShipArr[3].style.border = "none";
       allOneShipArr[3].remove();  
-    }
+    } else if(allTwoShipArr[0].style.border === "2px solid red"){
+      allTwoShipArr[0].style.border = "none";
+      allTwoShipArr[0].remove();  
+    } else if(allTwoShipArr[1].style.border === "2px solid red"){
+      allTwoShipArr[1].style.border = "none";
+      allTwoShipArr[1].remove();  
+    } else if(allTwoShipArr[2].style.border === "2px solid red"){
+      allTwoShipArr[2].style.border = "none";
+      allTwoShipArr[2].remove();
+    } else if(allThreeShipArr[0].style.border === "2px solid red"){
+      allThreeShipArr[0].style.border = "none";
+      allThreeShipArr[0].remove();
+    } else if(allThreeShipArr[1].style.border === "2px solid red"){
+      allThreeShipArr[1].style.border = "none";
+      allThreeShipArr[1].remove();
+    } else if(foreShipArr[0].style.border === "2px solid red"){
+      foreShipArr[0].style.border = "none";
+      foreShipArr[0].remove();
     
-  } else {
+  } else if(gridElement.classList === "ship_wrapper_oneShip") {
     gridElement.classList.remove("ship_wrapper_oneShip");
+    gridElement.classList.add("ship_wrapper");
+
   }}
   console.log('clik', newShip);
-}
+}}
+
 
 
 allShipWrapper.forEach((el)=> {
@@ -189,3 +279,174 @@ allShipWrapper.forEach((el)=> {
 })
 
 let Arr1 = [];
+
+const randomButton = document.getElementById('random');
+const playButton = document.getElementById('play');
+
+function rnd1() {
+  //ONE BOARD
+  allShipWrapper[0].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[24].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[89].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[16].classList.add("ship_wrapper_oneShip");
+//TWO BOARD
+  allShipWrapper[54].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[64].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[48].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[49].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[9].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[19].classList.add("ship_wrapper_oneShip");
+  //THREE BOARD
+  allShipWrapper[91].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[92].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[93].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[20].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[30].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[40].classList.add("ship_wrapper_oneShip");
+  //FOUR BOARD
+  allShipWrapper[66].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[67].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[68].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[69].classList.add("ship_wrapper_oneShip");
+
+}
+
+function rnd2() {
+  //ONE BOARD
+  allShipWrapper[0].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[9].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[90].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[99].classList.add("ship_wrapper_oneShip");
+//TWO BOARD
+  allShipWrapper[23].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[33].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[48].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[49].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[7].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[17].classList.add("ship_wrapper_oneShip");
+  //THREE BOARD
+  allShipWrapper[74].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[84].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[94].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[20].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[30].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[40].classList.add("ship_wrapper_oneShip");
+  //FOUR BOARD
+  allShipWrapper[66].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[67].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[68].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[69].classList.add("ship_wrapper_oneShip");
+
+}
+
+function rnd3() {
+  //ONE BOARD
+  allShipWrapper[11].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[16].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[69].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[99].classList.add("ship_wrapper_oneShip");
+//TWO BOARD
+  allShipWrapper[28].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[29].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[66].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[67].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[96].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[98].classList.add("ship_wrapper_oneShip");
+  //THREE BOARD
+  allShipWrapper[31].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[41].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[51].classList.add("ship_wrapper_oneShip");
+
+  allShipWrapper[72].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[73].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[74].classList.add("ship_wrapper_oneShip");
+  //FOUR BOARD
+  allShipWrapper[24].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[34].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[44].classList.add("ship_wrapper_oneShip");
+  allShipWrapper[54].classList.add("ship_wrapper_oneShip");
+
+}
+
+function rnd4() {
+  //ONE BOARD
+  allShipWrapperr2d2[11].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[16].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[69].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[99].classList.add("ship_wrapper_oneShip_r2d2");
+//TWO BOARD
+allShipWrapperr2d2[28].classList.add("ship_wrapper_oneShip_r2d2");
+allShipWrapperr2d2[29].classList.add("ship_wrapper_oneShip_r2d2");
+
+allShipWrapperr2d2[66].classList.add("ship_wrapper_oneShip_r2d2");
+allShipWrapperr2d2[67].classList.add("ship_wrapper_oneShip_r2d2");
+
+allShipWrapperr2d2[96].classList.add("ship_wrapper_oneShip_r2d2");
+allShipWrapperr2d2[98].classList.add("ship_wrapper_oneShip_r2d2");
+  //THREE BOARD
+  allShipWrapperr2d2[31].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[41].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[51].classList.add("ship_wrapper_oneShip_r2d2");
+
+  allShipWrapperr2d2[72].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[73].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[74].classList.add("ship_wrapper_oneShip_r2d2");
+  //FOUR BOARD
+  allShipWrapperr2d2[24].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[34].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[44].classList.add("ship_wrapper_oneShip_r2d2");
+  allShipWrapperr2d2[54].classList.add("ship_wrapper_oneShip_r2d2");
+
+}
+
+let randomNumberScale = 0;
+function randomNumber(max){
+  return Math.floor(Math.random() * max);
+}
+randomNumberScale = randomNumber(3);
+console.log('Random Number', randomNumberScale);
+
+function randomOne (){
+  dark.style.display = "flex";
+  choice.style.display = "none";
+  if(randomNumberScale === 0){
+    rnd1();
+    rnd4();
+  }
+  else if(randomNumberScale === 1){
+    rnd2();
+    rnd4();
+  }
+    else{
+      rnd3();
+      rnd4();
+  }
+}
+
+randomButton.addEventListener("click", _ => randomOne());
+
+//LOGIC SHUTTER
+
+function shutter ({target:gridElement}){
+  if(  choice.style.display == "none"){
+    if(gridElement.classList == "ship_wrapper_oneShip_r2d2")
+          {gridElement.classList.add("zero")
+    } else {
+      gridElement.classList.add("dj_One_shot")
+    }
+  }
+}
+
+allShipWrapperr2d2.forEach((el)=> {
+  el.addEventListener('click', shutter)
+  // console.log(el);
+})
+
